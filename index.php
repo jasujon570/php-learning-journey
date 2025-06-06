9491896b6e39d54d4491a084352e36513fe1ac6e
sujon <jasujon570@gmail.com>
2025-06-06 19:45:43 +0600

    Practice with PHP variables

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
-echo "My first file for GitHub!";
+$name = "kazi sujon";
+$age = 28;
+$city = "Gazipur";
+
+echo "Hello, my name is ".$name. ", I am ".$age. " years old and I live in ".$city. ".";
+
 
 ?>
